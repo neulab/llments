87c909aaa0538e6c2e13adf667504776cd6f7f60
Graham Neubig <neubig@gmail.com>
2023-11-22 16:03:20 -0500

    Update

--- llments/lm/hugging_face.py
+++ llments/lm/hugging_face.py
@@ -1,15 +1,28 @@
-
 from llments.lm.lm import LanguageModel
 
 
 class HuggingFaceLM(LanguageModel):
-
-    def sample(self, condition: str | None) -> str:
-        """Sample from the language model, possibly conditioned on a prefix."""
+    def sample(
+        self,
+        condition: str | None,
+        **kwargs,
+    ) -> str:
+        """Generate from the language model, possibly conditioned on a prefix."""
         raise NotImplementedError("This is not implemented yet.")
 
-    def fit(self, target: LanguageModel) -> LanguageModel:
-        """Fit the language model to a target language model's distribution."""
+    def fit(
+        self, target: LanguageModel, task_description: str | None = None
+    ) -> LanguageModel:
+        """Fit the language model to a target language model's distribution.
+
+        Args:
+            target: The language model that should be fitted to.
+            task_description: A task description that explains more about
+              what the language model that should be fit is doing (a prompt).
+
+        Returns:
+            The fitted language model.
+        """
         raise NotImplementedError("This is not implemented yet.")
 
 
