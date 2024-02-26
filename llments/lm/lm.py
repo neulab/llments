36041784857d47597efb205de5d53754068520f5
Rohan Modi <rohan.modi.1028@gmail.com>
2024-02-26 11:10:13 -0500

    Added abstract functions for LM and distmod

--- llments/lm/lm.py
+++ llments/lm/lm.py
@@ -26,3 +26,24 @@ class LanguageModel:
             The fitted language model.
         """
         ...
+
+    @abc.abstractmethod
+    def calculcate_probability(self, output: str) -> float:
+        """Calculate the probability of an output given the language model.
+
+        Args:
+            output (str): The output sequence for which the probability is calculated.
+
+        Returns:
+            float: The probability of output x given the language model.
+        """
+        ...
+
+    @abc.abstractmethod
+    def sample(self) -> str:
+        """Sample an output given the language model.
+
+        Returns:
+            str: A sampled output sequence from the language model.
+        """
+        ...
