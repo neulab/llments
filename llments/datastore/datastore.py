20c395e51742a26ea6f419218821323269fb3081
mihir86 <mstrmihir2@gmail.com>
2024-03-27 18:36:16 -0400

    minor fixes

--- llments/datastore/datastore.py
+++ llments/datastore/datastore.py
@@ -2,16 +2,30 @@ import abc
 
 class Datastore:
     @abc.abstractmethod
-    def __init__(self, input_jsonl, output_dir, encoder, fields, device):
-        """Initialize a Datastore object.
+    def __init__(self, index_path: str, document_path=None, index_encoder=None, fields=None, 
+                 to_faiss=False, device='cpu', delimiter="\n", docid_field=None, batch_size=64, max_length=256, 
+                 dimension=768, prefix=None, pooling=None, l2_norm=None, use_openai=False, rate_limit=3500):
+        """
+        Initializes a PyseriniDatastore object.
 
         Args:
-            input_jsonl (str): Path to the JSONL document file.
-            output_dir (str): Directory to store the encoded representations.
-            encoder (str): The type of document encoder to use.
-            fields (list): The document fields to be encoded.
-            device (str): The device to be used for encoding.
-        """
+            index_path (str): The path to store the generated index.
+            document_path (str, optional): The path to the document file. Defaults to None.
+            index_encoder (Any, optional): The type of document encoder. Defaults to None.
+            fields (List[str], optional): The document fields to be encoded. Defaults to ['text'].
+            to_faiss (bool, optional): Store as a FAISS index. Defaults to False.
+            device (str, optional): The device to be used for encoding. Defaults to 'cpu'.
+            delimiter (str, optional): Delimiter for document separation. Defaults to "\n".
+            docid_field (str, optional): Field in the document containing document id. Defaults to None.
+            batch_size (int, optional): Batch size for encoding. Defaults to 64.
+            max_length (int, optional): Maximum length of the input sequence. Defaults to 256.
+            dimension (int, optional): Dimensionality of the encoding. Defaults to 768.
+            prefix (str, optional): Prefix to add to each document. Defaults to None.
+            pooling (str, optional): Pooling strategy for document encoding. Defaults to None.
+            l2_norm (bool, optional): Whether to apply L2 normalization. Defaults to None.
+            use_openai (bool, optional): Whether to use OpenAI's encoder. Defaults to False.
+            rate_limit (int, optional): Rate limit for OpenAI API requests. Defaults to 3500.
+        """  
         pass
 
     @abc.abstractmethod
@@ -38,3 +52,4 @@ class Datastore:
             use_openai (bool, optional): Whether to use OpenAI's encoder. Defaults to False.
             rate_limit (int, optional): Rate limit for OpenAI API requests. Defaults to 3500.
         """
+        pass
