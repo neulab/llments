b6bdaea0454790ddde8ca35c6a4be93aa8a74197
mihir86 <mstrmihir2@gmail.com>
2024-03-31 22:38:26 -0400

    fixes

--- llments/datastore/datastore.py
+++ llments/datastore/datastore.py
@@ -22,6 +22,9 @@ class Datastore:
             query (str): The query string to search for.
             max_results (int): Maximum number of results to retrieve.
             query_encoder (str, optional): The type of query encoder to be used.
+            device (str, optional): Device to be used for encoding. Defaults to 'cpu'.
+            pooling (str, optional): Type of pooling to be used for encoding. Defaults to 'cls'.
+            l2_norm (bool, optional): Whether to apply L2 normalization to embeddings. Defaults to False.
 
         Returns:
             object: Retrieved result object.
