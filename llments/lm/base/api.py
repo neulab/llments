66868c7d72da3ee119acc18f467aa451223714fa
Aakriti Kinra <52823721+AakritiKinra@users.noreply.github.com>
2024-04-15 15:16:32 -0400

    API key changes

--- llments/lm/base/api.py
+++ llments/lm/base/api.py
@@ -1,6 +1,16 @@
 """Base class for API-Based Language Models."""
 
+import os
 from litellm import completion
+
+"""Set ENV variables.
+
+The language model's API key must be specified as an environment variable.
+The name of the environment variable depends on the model in use.
+For example: For OpenAI models the API key is set as OPENAI_API_KEY
+"""
+
+os.environ["API_KEY"] = "your-api-key"
 
 class APIBasedLM:
     """Base class for API-Based Language Models.
@@ -9,18 +19,15 @@ class APIBasedLM:
     using a specified API key and model name.
 
     Attributes:
-        api_key (str): The API key used for accessing the language model.
         model_name (str): The name of the language model.
     """
     
-    def __init__(self, api_key: str, model_name: str) -> None:
+    def __init__(self, model_name: str) -> None:
         """Initialize the APIBasedLM instance.
 
         Args:
-            api_key (str): The API key used for accessing the language model.
             model_name (str): The name of the language model.
         """
-        self.api_key = api_key
         self.model_name = model_name
     
     def generate_response(self, prompt: str) -> str:
