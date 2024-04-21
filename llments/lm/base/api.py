3f72d5e4ec6bf0b03e0631846ed4e2c3158ca0eb
AakritiKinra <aakritikinra@gmail.com>
2024-04-21 18:42:26 -0400

    LanguageModel inheritance 2

--- llments/lm/base/api.py
+++ llments/lm/base/api.py
@@ -7,39 +7,31 @@ from llments.lm.lm import LanguageModel
 
 class APIBasedLM(LanguageModel):
     """Base class for API-Based Language Models.
-
     Represents a language model that interacts with an API for generating responses.
-
     Usage:
     - Instantiate this class with the model name.
     - Set the API key of the language model as an environment 
       variable for secure access.
-
     Attributes:
         model_name (str): The name of the language model.
     """
     @abc.abstractmethod
     def __init__(self, model_name: str) -> None:
         """Initialize the APIBasedLM instance.
-
         Args:
             model_name (str): The name of the language model.
         """
         self.model_name = model_name
-        
+
     @abc.abstractmethod
     async def generate_response(self, prompt: str) -> str:
         """Generate a response based on the given prompt.
-
         This method sends a prompt to the language model API and retrieves
         the generated response.
-
         To handle potential rate limitations, this method uses the asynchronous
         version of the completion function, which allows for concurrent function calls.
-
         Args:
             prompt (str): The prompt for generating a response.
-
         Returns:
             str: The generated response from the language model.
         """
@@ -47,4 +39,3 @@ class APIBasedLM(LanguageModel):
             model=self.model_name,
             messages=[{"content": prompt, "role": "user"}]
         )
-        return str(response['choices'][0]['message']['content'])
